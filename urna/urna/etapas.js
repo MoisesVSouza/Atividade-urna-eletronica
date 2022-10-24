be6c37f90ce27d6d54265e031ca6242f6d6074ed
ThaynaSSilva <102416524+ThaynaSSilva@users.noreply.github.com>
2022-10-24 20:47:55 -0300

    Initial commit

---
+++ urna/urna/etapas.js
@@ -0,0 +1,24 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '01',
+                nome: 'Bob Esponja',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'bob_esponja.png', legenda: 'Presidente'}
+                ]
+            },
+            {
+                numero: '02',
+                nome: 'Lula Molusco',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'lula_molusco.png', legenda: 'Presidente'}
+                ]
+            }       
+        ]       
+    }
+]
